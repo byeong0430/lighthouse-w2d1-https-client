a7575d97e669393461ca894b4949f871dd55e7d8
byeong0430 <byeong.kim0430@gmail.com>
2018-06-11 18:51:30 +0000

    Completed Step.1

---
+++ read-html.js
@@ -0,0 +1,42 @@
+/* 
+  getAndPrintHTMLChunks() will be invoked multiple times.
+  Thus, https module should only required once.
+  Require a built-in https module
+*/
+const https = require('https');
+
+function getAndPrintHTMLChunks(){
+  let HTMLresult = '';
+  let requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  // Use GET (http verb) to send a request (requestOptions) and receive a response
+  https.get(requestOptions, function (response){
+    // response is a stream. Set the encoding to UTF-8
+    response.setEncoding('UTF8');
+
+    // Invoke the callback since data is available
+    response.on('data', function (chunk){
+      console.log('Chunk Received. Length: ', chunk.length);
+      // console.log each chunk of data and concatenate with a newline character
+      console.log(chunk.concat('\n'));
+      HTMLresult += chunk;
+    });
+
+    // Invoke the callback when there is an error
+    response.on('error', function (err){
+      console.log(err.stack);
+    });
+
+    // Invoke this callback when all of the data has been received
+    response.on('end', function (){
+      console.log('Final Result');
+      console.log(HTMLresult);
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
